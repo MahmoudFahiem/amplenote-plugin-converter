1c98a217f74be8204a32d5c4410ad7f192825bf1
Mahmoud Fahiem <xmahmoud92@gmail.com>
2023-12-13 23:06:48 +0200

    Init Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Amplenote Plugin Converter
+
+A converter that used to convert a JS file into Amplenote plugin API object structure.
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import path from "path";
+
+const srcPath = path.join(__dirname, "src");
+
+const main = () => {};
+
+if (require.main == module) main();
